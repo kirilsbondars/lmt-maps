1171c33a8ec67e03dc188b54418cc8f995e3b702
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2020-07-09 15:28:48 +0300

    Initial commit

--- public/test.php
+++ public/test.php
@@ -1,12 +1,26 @@
 <?php
 include_once("../src/initialize.php");
 
-$layers = LayerNew::find_all();
+//$layers = LayerNew::find_all();
+//
+//foreach($layers as $layer) {
+//    echo json_encode($layer);
+//}
+//
+//$layer = LayerNew::find_by_id(47);
+//echo json_encode($layer);
+//echo $layer->type();
+//
+$args = [];
+$args['name'] = "Katya's name";
+$args['path'] = 'some path';
+$args['style'] = '{"color" : "rainbow"}';
 
-foreach($layers as $layer) {
-    echo json_encode($layer);
+$layer = new LayerNew($args);
+$result = $layer->create();
+
+if($result) {
+    echo $layer->id . " " . $layer->name . " " . $layer->path . " " . $layer->style;
+} else {
+    //show errors
 }
-
-$layer = LayerNew::find_by_id(29);
-echo json_encode($layer);
-echo $layer->type();
--- src/classes/LayerNew.php
+++ src/classes/LayerNew.php
@@ -5,6 +5,7 @@ class LayerNew
 {
     // ----- START OF ACTIVE RECORD CODE ------
     static public $database;
+    static public $db_columns = ['id', 'name', 'path', 'style'];
 
     static public function set_database($database) {
         self::$database = $database;
@@ -53,6 +54,39 @@ class LayerNew
         }
         return $object;
     }
+
+    public function create() {
+        $attributes = $this->sanitized_attributes();
+        $sql = "INSERT INTO layer (";
+        $sql .= join(', ', array_keys($attributes));
+        $sql .= ") VALUES ('";
+        $sql .= join("' , '", array_values($attributes));
+        $sql .= "')";
+        $result = self::$database->query($sql);
+        if($result) {
+            $this->id = self::$database->insert_id;
+        }
+        return $result;
+    }
+
+    // Properties which have database columns, excluding ID
+    public function attributes() {
+        $attributes = [];
+        foreach(self::$db_columns as $column) {
+            if($column == 'id') { continue; }
+            $attributes[$column] = $this->$column;
+        }
+        return $attributes;
+    }
+
+    protected function sanitized_attributes() {
+        $sanitized = [];
+        foreach($this->attributes() as $key => $value) {
+            $sanitized[$key] = self::$database->escape_string($value);
+        }
+        return $sanitized;
+    }
+
     // ----- END OF ACTIVE RECORD CODE ------
 
     public $id, $name, $path, $style, $type;
